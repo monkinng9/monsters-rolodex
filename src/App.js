e8c57c92bd662a86ba4d12b61befe5035ef9ac16
Chayut Tumon <53258436+monkinng9@users.noreply.github.com>
2022-05-31 23:56:34 +0700

    30. Monsters Rolodex - setState and Secondary Callback

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Chayut'
+      name: {firstName: 'Chayut', lastName: 'Tumon'},
+      company: 'ZTM',
     }
   }
 
@@ -17,10 +18,17 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello {this.state.name}</p>
+          <p>Hello {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}</p>
           <button onClick={() => {
-            this.setState({name: 'Andrei'});
-          }}>Change Name</button>
+              this.setState(
+                () => {
+                  return {
+                    name: {firstName: 'Andrei', lastName: 'Neaogie'}
+                  };
+                }
+              );
+            }
+          }>Change Name</button>
         </header>
       </div>
     );
