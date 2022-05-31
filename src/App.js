7dbf2b4ca84e30efb51199c08ab43da0b9c30599
Chayut Tumon <53258436+monkinng9@users.noreply.github.com>
2022-05-31 23:33:59 +0700

    27. Monsters Rolodex - Component State

--- src/App.js
+++ src/App.js
@@ -4,23 +4,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Chayut'
+    }
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello Motherfucker!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Hello {this.state.name}</p>
+          <button>Change Name</button>
         </header>
       </div>
     );
